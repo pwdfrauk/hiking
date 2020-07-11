29af5eaaba4495ec2c33bd8fd8ae38904d6fd4e6
Your Name <you@example.com>
2020-07-11 21:22:52 +0600

    adding file

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+
+/*-=-=-=-=-=-=- owl carousel -=-=-==-=-=-=-=-=-=-=*/
+$('.menu-icon').on('click', function(){
+    $('.menu').slideToggle();
+})
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
